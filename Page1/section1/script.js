f2ec6f365e398c3c74d7780cdd664b600b47788d
Omar Abbas <115028809+Omar7001-B@users.noreply.github.com>
2023-07-29 02:17:09 +0300

    Add files via upload

---
+++ Page1/section1/script.js
@@ -0,0 +1,155 @@
+// width1 = s1-input1
+// width2 = s1-input2
+// height = s1-input3
+// area = s2-input1
+// price = s2-input2
+let width1 = document.querySelector(".input-s1 input");
+let width2 = document.querySelector(".input-s2 input");
+let height = document.querySelector(".input-s3 input");
+let area1 = document.querySelector(".selector1");
+let area2 = document.querySelector(".selector2");
+let price = document.querySelector(".s2 div input");
+let average_result = document.querySelector("#average-result");
+let totalArea_result = document.querySelector("#totalArea-result");
+let price_result = document.querySelector("#price-result");
+let area_result1 = document.querySelector("#area-result1");
+let area_result2 = document.querySelector("#area-result2");
+let area_result3 = document.querySelector("#area-result3");
+let width1_result = document.querySelectorAll(".width1_result");
+let width2_result = document.querySelectorAll(".width2_result");
+let height_result = document.querySelectorAll(".height_result");
+
+Number.prototype.toFixedNoRounding = function (n) {
+  const reg = new RegExp("^-?\\d+(?:\\.\\d{0," + n + "})?", "g");
+  const a = this.toString().match(reg)[0];
+  const dot = a.indexOf(".");
+  if (dot === -1) {
+    // integer, insert decimal dot and pad up zeros
+    return a + "." + "0".repeat(n);
+  }
+  const b = n - (a.length - dot) + 1;
+  return b > 0 ? a + "0".repeat(b) : a;
+};
+
+function last(type, num) {
+  let number = (type.value * 100) / 14.7916666667 / 24;
+  let num1 = Math.floor(number);
+  let num2 = Math.floor(getfloor(number) * 24);
+  let num3 = getfloor(getfloor(number) * 24).toFixed(2);
+  return num == "num3"
+    ? num1 || 0
+    : num == "num2"
+    ? num2 || 0
+    : num == "num1"
+    ? num3 || 0
+    : "";
+}
+
+function other() {
+  if (area1.options[area1.selectedIndex].text != "اخر") {
+    area2.value = area1.options[area1.selectedIndex].text;
+  } else {
+    area2.value = "";
+  }
+}
+
+function average() {
+  return (+width1.value + +width2.value) / 2;
+}
+function totalArea() {
+  return average() * height.value;
+}
+function result() {
+  return totalArea() / area2.value;
+}
+function acre() {
+  return result() / 24;
+}
+function getfloor(num) {
+  let tostr = num.toString();
+  if (tostr.includes(".")) {
+    let result = tostr.slice(tostr.indexOf("."));
+    return Number(result);
+  } else {
+    return 0;
+  }
+}
+function carats() {
+  return getfloor(acre()) * 24;
+}
+function shares() {
+  return getfloor(carats()) * 24;
+}
+function getprice() {
+  return price.value * result();
+}
+function isFull() {
+  width1_num1 = last(width1, "num1");
+  width1_num2 = last(width1, "num2");
+  width1_num3 = last(width1, "num3");
+  width2_num1 = last(width2, "num1");
+  width2_num2 = last(width2, "num2");
+  width2_num3 = last(width2, "num3");
+  height_num1 = last(height, "num1");
+  height_num2 = last(height, "num2");
+  height_num3 = last(height, "num3");
+  if (width1_num1 == 1) {
+    width1_num3 += 1;
+    width1_num2 = 0;
+    width1_num1 = 0;
+  }
+  if (width2_num1 == 1) {
+    width2_num3 += 1;
+    width2_num2 = 0;
+    width2_num1 = 0;
+  }
+  if (height_num1 == 1) {
+    height_num3 += 1;
+    height_num2 = 0;
+    height_num1 = 0;
+  }
+  width1_result[0].innerText = width1_num1;
+  width1_result[1].innerText = width1_num2;
+  width1_result[2].innerText = width1_num3;
+  width2_result[0].innerText = width2_num1;
+  width2_result[1].innerText = width2_num2;
+  width2_result[2].innerText = width2_num3;
+  height_result[0].innerText = height_num1;
+  height_result[1].innerText = height_num2;
+  height_result[2].innerText = height_num3;
+}
+
+function calculate() {
+  totalArea_result.innerText = (totalArea() || 0).toFixed(2);
+  price_result.innerText = Math.floor(getprice() || 0).toLocaleString();
+  area_result1.innerText = Math.floor(acre() || 0);
+  area_result2.innerText = Math.floor(carats() || 0);
+  area_result3.innerText = (shares() || 0).toFixedNoRounding(2);
+  isFull();
+}
+
+function clearall() {
+  let inputs = document.querySelectorAll("input");
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].className == `selector2`) {
+      continue;
+    }
+    inputs[i].value = "";
+  }
+}
+// resize event listener to detect change in screen height
+// let input = document.querySelector("input");
+// let nav = document.querySelector(".nav");
+// let deviceHeight = window.innerHeight;
+// window.addEventListener("click", ()=>{
+//   if(window.innerHeight != deviceHeight){
+//     nav.style.display = "none"
+//   }
+// })
+// window.visualViewport.addEventListener("resize", () =>
+//   window.getComputedStyle(nav).display == "none"
+//     ? (nav.style.display = "flex")
+//     : window.getComputedStyle(nav).display == "flex"
+//     ? (nav.style.display = "none")
+//     : ""
+// );
---
+++ addnav.js
@@ -0,0 +1,58 @@
+document.body.style="margin:0px;"
+let newNav = document.createElement("div");
+newNav.style = "position: fixed; width: 100%; height: 60px; background-color: rgb(168,255,146); bottom: 0px; display: flex; flex-flow: row-reverse; justify-content: space-around; align-items: center; box-sizing: border-box;margin:0px;"
+newNav.className = "nav";
+newNav.setAttribute("dir", "rtl");
+let url_nav = location.pathname.split("Ard");
+let path_nav = url_nav[url_nav.length - 1];
+let result_nav = "";
+for(let i = 1;i < countCharOccurrences(path_nav, "/"); i++){
+    result_nav+= "../"
+}
+console.log(result_nav)
+let a_style = `height: 90%; text-decoration: none; display: flex; flex-direction: column; justify-content: space-around; align-items: center; color:black;`
+let img_style = ` width: 28px; height: 25px; box-sizing: border-box;`
+let p_style = ` margin: 0px; padding: 0px;`
+newNav.innerHTML = ` 
+    <a href="help2/index.html" class="nav-box" style="${a_style}">
+      <img src="${result_nav}imgs/nav1.png" alt="nav1" style="${img_style}" />
+      <p style="${p_style}">كسور</p>
+    </a>
+    <a class="nav-box" onclick="history.back()" style="${a_style}">
+      <img src="${result_nav}imgs/nav2.png" alt="nav1" style="${img_style}" />
+      <p style="${p_style}">رجوع</p>
+    </a>
+    <a href="help/index.html" class="nav-box" style="${a_style}">
+      <img src="${result_nav}imgs/nav3.png" alt="nav1" style="${img_style}" />
+      <p style="${p_style}">مساعدة</p>
+    </a>
+    <a href="../index.html" class="nav-box" style="${a_style}">
+      <img src="${result_nav}imgs/nav4.png" alt="nav1" style="${img_style}" />
+      <p style="${p_style}">الرئسية</p>
+    </a> `
+
+
+document.body.appendChild(newNav)
+
+
+function countCharOccurrences(string, char) {
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+    if (string[i] === char) {
+        count++;
+    }
+}
+return count;
+}
+
+
+let input = document.querySelector("input");
+let nav = document.querySelector(".nav");
+
+window.visualViewport.addEventListener("resize", () =>
+  window.getComputedStyle(nav).display == "none"
+    ? (nav.style.display = "flex")
+    : window.getComputedStyle(nav).display == "flex"
+    ? (nav.style.display = "none")
+    : ""
+);
