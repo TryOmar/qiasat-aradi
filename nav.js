9704a248a6d8d2f6b27603d7e1a21ee49f0045ad
dalalarady <dalalarady@gmail.com>
2026-07-07 02:41:45 +0300

    chore: preserve user choice to not load outdoor-mode globally

--- nav.js
+++ nav.js
@@ -1,22 +1,3 @@
-// Automatically load outdoor-mode.js dynamically from root
-(function() {
-  let root = "./";
-  const scripts = document.querySelectorAll('script[src]');
-  for (const s of scripts) {
-    const m = s.src.match(/^(.*\/)nav\.js$/);
-    if (m) {
-      root = m[1];
-      break;
-    }
-  }
-  if (!document.getElementById("outdoor-mode-script-dynamic")) {
-    const script = document.createElement("script");
-    script.id = "outdoor-mode-script-dynamic";
-    script.src = root + "outdoor-mode.js";
-    document.head.appendChild(script);
-  }
-})();
-
 // resize event listener to detect change in screen height
 let input = document.querySelector("input");
 let nav = document.querySelector(".nav");
